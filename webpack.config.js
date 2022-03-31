33e06e6074175a8f8bbd0fa7eb02b7be95156198
mahmoudadel54 <mahmoodmadel@gmail.com>
2022-03-31 21:10:17 +0200

    shared approach in handling entry point and package bundling duplication

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
-const path = require('path');
-module.exports={
-    mode:'development',
-    entry:{
-        index:"./src/index.js",
-        other:"./src/other/other.js"
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: {
+    index: {
+      import: "./src/index.js",
+      dependOn: "shared",
     },
-    output:{
-    filename:"[name].bundle.js",            //[name] points to filename in that existing in entry
-    path:path.resolve(__dirname, 'dist')
-    }
-}
+    other: {
+      import: "./src/other/other.js",
+      dependOn: "shared",
+    },
+    shared: "lodash",
+  },
+  output: {
+    filename: "[name].bundle.js", //[name] points to filename in that existing in entry
+    path: path.resolve(__dirname, "dist"),
+  },
+};
