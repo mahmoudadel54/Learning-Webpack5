5a85958efe54306a8f3c72193b53f8e4e2639b98
mahmoudadel54 <mahmoodmadel@gmail.com>
2022-04-07 13:46:50 +0200

    2nd appraoch: SplitChunckPlugin appraoch to prevent module duplication

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
 const path = require("path");
+/**
+ * 1st way to prevent duplication: shared approach
+*/
+// module.exports = {
+//   mode: "development",
+//   entry: {
+//     index: {
+//       import: "./src/index.js",
+//       dependOn: "shared",
+//     },
+//     other: {
+//       import: "./src/other/other.js",
+//       dependOn: "shared",
+//     },
+//     shared: "lodash",
+//   },
+//   output: {
+//     filename: "[name].bundle.js", //[name] points to filename in that existing in entry
+//     path: path.resolve(__dirname, "dist"),
+//   },
+// };
+
+//2nd appraoch: SplitChunckPlugin appraoch
+const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    index: {
-      import: "./src/index.js",
-      dependOn: "shared",
-    },
-    other: {
-      import: "./src/other/other.js",
-      dependOn: "shared",
-    },
-    shared: "lodash",
+    index: "./src/index.js",
+    other: "./src/other/other.js"
+  },
+  optimization:{
+    splitChunks:{
+      chunks:'all'
+    }
   },
   output: {
     filename: "[name].bundle.js", //[name] points to filename in that existing in entry
