aea429c998391b568e18f3ae5f1163d9f2e5c5b5
mahmoudadel54 <mahmoodmadel@gmail.com>
2022-04-07 14:55:59 +0200

    asset managements css, images, fonts, ..etc

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,23 @@ module.exports = {
     filename: "index.js", //[name] points to filename in that existing in entry
     path: path.resolve(__dirname, "dist"),
   },
+    module: {
+    rules: [
+      //css rule
+      {
+        test: /\.css$/i,      //any line in code includes .css will handle it directly via webpack
+        use: ['style-loader', 'css-loader'],    
+      },
+        //images rule
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type:'asset/resource',      //no need to external laoder package, just use built-in type
+        },
+          //fonts rule
+       {
+        test: /\.(woff|woff2|eot|tff|otf)$/i,
+        type:'asset/resource',      //no need to external laoder package, just use built-in type
+      },
+    ],
+  },
 };
